41cec22a5e971b577efd858c3e8a6a7852910ce6
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-14 15:41:52 +0800

    Added function that prints Try and Try

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ console.log("This is a server code...")
 console.log("Geneva Amaya Rivas")
 console.log("Keep on fighting Jhen...")
 >>>>>>> f7e3f32e14fae27000dc64f80437fa8ed2e07e46
+console.log("Try and try until you succeed.")
